6607fc404094ec3bffecaac06bed95a552de3a94
atkingtornado <atkingtornado@gmail.com>
2017-04-19 12:29:39 -0500

    changed data source to SHARP, some other fixes

--- js/leaflet-providers.js
+++ js/leaflet-providers.js
@@ -224,9 +224,7 @@
 			url: '//stamen-tiles-{s}.a.ssl.fastly.net/{variant}/{z}/{x}/{y}.{ext}',
 			options: {
 				attribution:
-					'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ' +
-					'<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; ' +
-					'Map data {attribution.OpenStreetMap}',
+					'',
 				subdomains: 'abcd',
 				minZoom: 0,
 				maxZoom: 20,
